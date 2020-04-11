ae66ae994fa0720d1496a2f07fbcb636939f6c5c
Mirko <mirko.kunze@web.de>
2020-04-11 12:07:53 +0200

    added main libs, basic html and broker.js

---
+++ build/broker.js
@@ -0,0 +1,37 @@
+var EventChannel = /** @class */ (function () {
+    function EventChannel() {
+        this.handlers = new Set();
+    }
+    EventChannel.prototype.addHandler = function (handler) {
+        this.handlers.add(handler);
+    };
+    EventChannel.prototype.removeHandler = function (handler) {
+        this.handlers["delete"](handler);
+    };
+    EventChannel.prototype.fire = function (event) {
+        this.handlers.forEach(function (handler) {
+            handler(event);
+        });
+    };
+    EventChannel.prototype.fetch = function (request) {
+        var responses = [];
+        this.handlers.forEach(function (handler) {
+            var response = handler(request);
+            if (response !== undefined) {
+                responses.push(response);
+            }
+        });
+        return responses;
+    };
+    return EventChannel;
+}());
+var broker = {};
+broker.newChannel = function (name) {
+    if (broker.hasOwnProperty(name)) {
+        throw new Error('channel "' + name + '" already exists');
+    }
+    else {
+        broker[name] = new EventChannel();
+    }
+};
+//# sourceMappingURL=broker.js.map
---
+++ src/broker.ts
@@ -0,0 +1,41 @@
+
+type EventHandler = (event: any) => any
+
+class EventChannel{
+	private handlers = new Set<EventHandler>()
+
+	addHandler(handler: EventHandler){
+		this.handlers.add(handler)
+	}
+
+	removeHandler(handler: EventHandler){
+		this.handlers.delete(handler)
+	}
+
+	fire(event: any){
+		this.handlers.forEach(function(handler){
+			handler(event)
+		})
+	}
+
+	fetch(request: any){
+		let responses:any[] = []
+		this.handlers.forEach(function(handler){
+			let response:any = handler(request)
+			if(response !== undefined){
+				responses.push(response)
+			}
+		})
+		return responses
+	}
+}
+
+let broker:any = {}
+broker.newChannel = function(name:string){
+	if(broker.hasOwnProperty(name)){
+		throw new Error('channel "' + name + '" already exists')
+	}
+	else{
+		broker[name] = new EventChannel()
+	}
+}
