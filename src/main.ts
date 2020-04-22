8c340c4247ad2a1133d58f02e9b465008e59ce07
Mirko <mirko.kunze@web.de>
2020-04-22 20:54:03 +0200

    added stats display

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 
 import {GraphicsManager} from "./graphics/graphicsmanager"
-import * as THREE from 'three'
+import * as THREE from "three"
+import Stats from "stats-js"
 
 let gm = new GraphicsManager("rendertarget")
+
+let stats = new Stats()
+stats.showPanel(0)
+document.body.appendChild(stats.dom)
 
 gm.setup()
 gm.setBackground(new THREE.Color("skyblue"))
@@ -10,12 +15,13 @@ gm.setBackground(new THREE.Color("skyblue"))
 let cube = new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshNormalMaterial())
 gm.addToScene(cube)
 
-function animate(){
+function animate(time:number){
 	requestAnimationFrame(animate)
 	cube.rotation.x += 0.01
 	cube.rotation.y += 0.02
 	cube.rotation.z += 0.03
 	gm.render()
+	stats.update()
 }
-animate()
+animate(0)
 
