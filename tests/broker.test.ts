76a338ebc23332166317e81f4c0405687ad2f924
Horst Boehse <daniel123@bossmail.de>
2020-04-26 19:45:21 +0200

    Write first unit test using mocha

---
+++ tests/broker.test.ts
@@ -0,0 +1,27 @@
+import 'mocha'
+import {expect} from 'chai'
+import {broker, EventChannel} from '../src/broker'
+
+describe('Test broker', () => {
+	it('fire', () => {
+		
+		broker.newChannel('myChannel')
+		
+		let myChannel:EventChannel = broker['myChannel']
+
+		let receivedEvent:any;
+
+		myChannel.addHandler(e => {
+			receivedEvent = e
+		})
+
+		myChannel.fire( {
+			name: 'eventName',
+			data: 3
+		})
+
+		expect(receivedEvent.name).to.equal('eventName')
+		expect(receivedEvent.data).to.equal(3)
+
+	})
+})
