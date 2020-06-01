f9d7a52f8bef426ede718ba59a0455acd0edfe49
Mirko <mirko.kunze@web.de>
2020-06-01 13:36:56 +0200

    using the working asset loading management :)

--- src/ui_main.ts
+++ src/ui_main.ts
@@ -1,22 +1,31 @@
 
 import {ThreeGraphics} from "./graphics/threegraphics/threegraphics"
 import {ModelConfig} from "./graphics/model"
+import {Checklist} from "./checklist"
 
 let graphics = new ThreeGraphics(document.getElementById("rendertarget") as HTMLCanvasElement)
 
-const gliderAsset = graphics.loadAsset<"model">({kind:"model", file:"glider/glider.gltf"})
-const gliderCfg:ModelConfig = {
-	kind:"model",
-	asset:gliderAsset
-}
+function start(){
 
-setTimeout(function(){
+	const gliderCfg:ModelConfig = {
+		kind:"model",
+		asset:gliderAsset
+	}
+
 	graphics.addObject(gliderCfg)
-}, 1000)
 
-function animate(time:number){
+	function animate(time:number){
+		requestAnimationFrame(animate)
+		graphics.update(time)
+	}
 	requestAnimationFrame(animate)
-	graphics.update(time)
+	
 }
-requestAnimationFrame(animate)
 
+let checklist = new Checklist({onComplete:start})
+
+let loadGlider = checklist.newItem()
+const gliderAsset = graphics.loadAsset<"model">(
+	{kind:"model", file:"glider/glider.gltf"},
+	loadGlider.check
+)
