52dcf083b58f3b602f704c3a0a0c57a3529241bb
Horst Boehse <daniel123@bossmail.de>
2020-10-26 20:57:32 +0100

    Added unit tests for DefaultControllerManager

--- tests/adapters/controller/defaultcontrollermanager.test.ts
+++ tests/adapters/controller/defaultcontrollermanager.test.ts
@@ -1,19 +1,30 @@
-import 'mocha'
-import { JSDOM } from 'jsdom'
+import 'jsdom-global/register'
+import {describe, it, before} from 'mocha'
 import { expect } from 'chai'
 import { DefaultControllerManager } from 'adapters/controller/defaultcontrollermanager'
+import { Controller } from 'domain/controller/controller'
+import { ControllerManager } from 'domain/controller/controllermanager'
+import { Keyboard } from 'adapters/controller/keyboard'
 
 describe('Test DefaultControllerManager', () => {
 
-	let window = new JSDOM('...').window;
-	let document: Document = window.document;
-	let controllerManager: DefaultControllerManager
+	let controllerManager: ControllerManager
 
 	before(() => {
 		controllerManager = new DefaultControllerManager()
 	})
 
-	it('Initial State', () => {
+	it('Test keyboard is immediately connected', () => {
+		let connectedControllers:Controller[] = new Array();
+		controllerManager.addConnectionCallback(newController => connectedControllers.push(newController))
+		expect(connectedControllers).to.have.lengthOf(1)
+		expect(connectedControllers[0]).to.be.instanceOf(Keyboard)
+	})
+
+	it('Remove connection callback', () => {
+		let callback = (controller:Controller) => {}
+		controllerManager.addConnectionCallback(callback)
+		controllerManager.removeConnectionCallback(callback)
 	})
 
 	
