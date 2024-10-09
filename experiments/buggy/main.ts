7f6dbe7bf30abda32b3339e5daf9ede9bb917406
mqnc <mirko.kunze@web.de>
2024-10-09 23:52:36 +0200

    worked on buggy experiment

--- experiments/buggy/main.ts
+++ experiments/buggy/main.ts
@@ -13,45 +13,93 @@ const gui = new dat.GUI();
 
 const world = new p2.World({ gravity: [0, 0] })
 
-const chassisBody = new p2.Body({ mass: 1, damping: 0.3, angularDamping: 0.3 })
+const chassisBody = new p2.Body({
+	mass: 1,
+	damping: 0.3,
+	angularDamping: 0.3
+})
 const boxShape = new p2.Box({ width: 1.0, height: 0.5 })
 chassisBody.addShape(boxShape)
 world.addBody(chassisBody)
 
+let chassisParams = {
+	mass: 1,
+	momentOfInertia: 0.1
+}
+
 let wheels = [
 	{
-		position: vec2.fromValues(0.5, 0),
+		label: "FrontWheel",
+		maxSteeringAngle: 30, // deg
+		steeringSpeed: 2.0, // rad/s
+		lateralDamping: 20, // reaction force over sliding velocity (up to gripping force limit)
+		maxGripForce: 30, // N
+		slidingForceFraction: 0.75, // reaction force when sliding faster
+		motorForce: 0, // N between wheel and ground
+		brakeForce: 0,
+		position: 0.5,
+
 		steering: 0,
 		thrust: 0,
+		braking: 0,
 		locked: false,
 		mesh: undefined as any
 	},
 	{
-		position: vec2.fromValues(-0.5, 0),
+		label: "RearWheel",
+		maxSteeringAngle: 0, // deg
+		steeringSpeed: -1.0, // rad/s
+		lateralDamping: 20, // reaction force over sliding velocity (up to gripping force limit)
+		maxGripForce: 30, // N
+		slidingForceFraction: 0.75, // reaction force when sliding faster
+		motorForce: 15, // N between wheel and ground
+		brakeForce: 30,
+		position: -0.5,
+
 		steering: 0,
 		thrust: 0,
+		braking: 0,
 		locked: false,
 		mesh: undefined as any
 	}
 ]
 
-let params = {
-	maxSteeringAngle: 30, // deg
-	steeringSpeed: 0.03, // rad/s
-	wheelDamping: 50, // reaction force over sliding velocity (up to gripping force limit)
-	wheelMaxGrippingForce: 30, // force units :P
-	wheelSlidingForceReduction: 0.75, // reaction force when sliding faster
-	wheelThrustForce: 15,
-	wheelBrakeForce: 30
+let options = {
+	syncFrictionParams: true,
+	slowMo: 1,
+	superSample: 10
 }
 
-gui.add(params, 'maxSteeringAngle', 0, 90)
-gui.add(params, 'steeringSpeed', 0, 0.2)
-gui.add(params, 'wheelDamping', 0, 100)
-gui.add(params, 'wheelMaxGrippingForce', 0, 100)
-gui.add(params, 'wheelSlidingForceReduction', 0, 1)
-gui.add(params, 'wheelThrustForce', 0, 100)
-gui.add(params, 'wheelBrakeForce', 0, 100)
+let carFolder = gui.addFolder("Car")
+carFolder.add(chassisParams, 'mass', 0.1, 20)
+carFolder.add(chassisParams, 'momentOfInertia', 0.01, 5)
+
+for (let i = 0; i < 2; i++) {
+	let wheel = wheels[i]
+	let otherWheel = wheels[1 - i]
+
+	let wheelFolder = gui.addFolder(wheel.label)
+	wheelFolder.add(wheel, 'maxSteeringAngle', 0, 90)
+	wheelFolder.add(wheel, 'steeringSpeed', -10, 10)
+	wheelFolder.add(wheel, 'motorForce', 0, 100)
+	wheelFolder.add(wheel, 'brakeForce', 0, 100)
+	wheelFolder.add(wheel, 'lateralDamping', 0, 100).onChange((value) => {
+		if (options.syncFrictionParams) { otherWheel.lateralDamping = value }
+	}).listen()
+	wheelFolder.add(wheel, 'maxGripForce', 0, 100).onChange((value) => {
+		if (options.syncFrictionParams) { otherWheel.maxGripForce = value }
+	}).listen()
+	wheelFolder.add(wheel, 'slidingForceFraction', 0, 1).onChange((value) => {
+		if (options.syncFrictionParams) { otherWheel.slidingForceFraction = value }
+	}).listen()
+	wheelFolder.add(wheel, 'position', -3, 3)
+}
+
+let optionsFolder = gui.addFolder("Options")
+optionsFolder.add(options, 'slowMo', 0.1, 2)
+optionsFolder.add(options, 'syncFrictionParams')
+optionsFolder.add(options, 'superSample', 1, 100, 1)
+
 
 const keys: any = {
 	ArrowLeft: 0,
@@ -60,18 +108,8 @@ const keys: any = {
 	ArrowDown: 0
 }
 const maxSteer = Math.PI / 5
-document.addEventListener('keydown', function (evt) {
-	keys[evt.key] = 1
-	onInputChange()
-})
-document.addEventListener('keyup', function (evt) {
-	keys[evt.key] = 0
-	onInputChange()
-})
-
-function onInputChange() {
-
-}
+document.addEventListener('keydown', (evt) => { keys[evt.key] = 1 })
+document.addEventListener('keyup', (evt) => { keys[evt.key] = 0 })
 
 const planeSize = 1000;
 const divisions = 1000;
@@ -79,11 +117,11 @@ const gridHelper = new THREE.GridHelper(planeSize, divisions);
 gridHelper.rotation.x = Math.PI / 2
 scene.add(gridHelper);
 
-const chassis = new THREE.Mesh(
+const chassisMesh = new THREE.Mesh(
 	new THREE.BoxGeometry(1.6, 0.8, 0.1),
 	new THREE.MeshBasicMaterial({ color: 0x101010 })
 )
-scene.add(chassis)
+scene.add(chassisMesh)
 
 const centerOfRotationMarker = new THREE.Mesh(
 	new THREE.SphereGeometry(0.1),
@@ -96,8 +134,8 @@ for (let wheel of wheels) {
 		new THREE.CylinderGeometry(0.3, 0.3, 0.1),
 		new THREE.MeshBasicMaterial({ color: 0x0000ff })
 	)
-	chassis.add(wheelMesh)
-	wheelMesh.position.set(wheel.position[0], wheel.position[1], 0)
+	chassisMesh.add(wheelMesh)
+	wheelMesh.position.set(wheel.position, 0, 0)
 	const wheelAxle = new THREE.Mesh(
 		new THREE.CylinderGeometry(0.02, 0.02, 20),
 		new THREE.MeshBasicMaterial({ color: 0x0000ff })
@@ -143,136 +181,166 @@ function circle(position: vec2, radius: number, color: THREE.Color) {
 let lastTime = 0
 function update(time: number) {
 
-	let maxSteeringAngle = params.maxSteeringAngle * Math.PI / 180
-	let steeringSpeed = params.steeringSpeed
-	let wheelDamping = params.wheelDamping
-	let wheelMaxGrippingForce = params.wheelMaxGrippingForce
-	let wheelSlidingForceReduction = params.wheelSlidingForceReduction
-	let wheelThrustForce = params.wheelThrustForce
-	let wheelBrakeForce = params.wheelBrakeForce
-
-	let wheelSlipThresholdSpeed = wheelMaxGrippingForce / wheelDamping
-	let wheelSlidingForce = wheelMaxGrippingForce * wheelSlidingForceReduction
-
+	let elapsedTime = lastTime == 0 ? 0.01 : time - lastTime
+	lastTime = time
 
 	scene.children = scene.children.filter(child => !(child.userData.deleteMe));
 
-	if (keys.ArrowLeft || keys.ArrowRight) {
-		wheels[0].steering += (keys.ArrowLeft - keys.ArrowRight) * steeringSpeed
-		wheels[0].steering = Math.max(-maxSteeringAngle, Math.min(maxSteeringAngle, wheels[0].steering))
+	for (let i = 0; i < options.superSample; i++) {
+
+		let dt = elapsedTime * options.slowMo / options.superSample
+
+		world.step(dt)
+
+		for (let wheel of wheels) {
+
+			let maxSteeringAngle = wheel.maxSteeringAngle * Math.PI / 180
+			let steeringSpeed = wheel.steeringSpeed
+			let lateralDamping = wheel.lateralDamping
+			let maxGripForce = wheel.maxGripForce
+			let slidingForceFraction = wheel.slidingForceFraction
+			let motorForce = wheel.motorForce
+			let brakeForce = wheel.brakeForce
+
+			let slipThresholdSpeed = maxGripForce / lateralDamping
+			let slidingForce = maxGripForce * slidingForceFraction
+
+			chassisBody.mass = chassisParams.mass
+			chassisBody.inertia = chassisParams.momentOfInertia
+			chassisBody.invMass = 1 / chassisParams.mass
+			chassisBody.invInertia = 1 / chassisParams.momentOfInertia
+
+			if (keys.ArrowLeft || keys.ArrowRight) {
+				wheel.steering += (keys.ArrowLeft - keys.ArrowRight) * steeringSpeed * dt
+				wheel.steering = Math.max(-maxSteeringAngle, Math.min(maxSteeringAngle, wheel.steering))
+			}
+			else if (wheel.steering > 0) {
+				wheel.steering = Math.max(0, wheel.steering - Math.abs(steeringSpeed) * dt)
+			}
+			else if (wheel.steering < 0) {
+				wheel.steering = Math.min(0, wheel.steering + Math.abs(steeringSpeed) * dt)
+			}
+			wheel.thrust = keys.ArrowUp * motorForce
+			wheel.braking = keys.ArrowDown * brakeForce
+
+			const wheelPosition = vec2.fromValues(wheel.position, 0)
+			const wheelAngle = chassisBody.angle + wheel.steering
+
+			let radius = vec2.create();
+			vec2.rotate(radius, wheelPosition, [0, 0], chassisBody.angle);
+			const turningComponent = vec2.fromValues(
+				-chassisBody.angularVelocity * radius[1],
+				chassisBody.angularVelocity * radius[0]
+			)
+
+			let globalWheelPosition = vec2.create()
+			chassisBody.toWorldFrame(
+				globalWheelPosition as [number, number],
+				wheelPosition as [number, number]
+			)
+
+			const motion = vec2.add(vec2.create(), chassisBody.velocity, turningComponent)
+
+			const forwardDirection = vec2.fromValues(
+				Math.cos(wheelAngle),
+				Math.sin(wheelAngle),
+			)
+
+			const forwardVelocity = vec2.dot(motion, forwardDirection)
+			let drive = wheel.thrust
+			if (wheel.braking) {
+				if (Math.abs(forwardVelocity) > 0.03) {
+					drive = -Math.sign(forwardVelocity) * wheel.braking
+				}
+			}
+
+			const axisDirection = vec2.fromValues(
+				-forwardDirection[1],
+				forwardDirection[0]
+			)
+
+			const sideVelocity = vec2.dot(motion, axisDirection)
+			const sideSpeed = Math.abs(sideVelocity)
+
+			let forwardForceAbs = Math.abs(drive)
+			let sideForceAbs = sideSpeed < slipThresholdSpeed ?
+				lateralDamping * sideSpeed : slidingForce
+
+			let sliding = false
+
+			let maxWheelForceAbs = maxGripForce
+			if (
+				sideSpeed > slipThresholdSpeed
+				|| forwardForceAbs ** 2 + sideForceAbs ** 2 > maxGripForce ** 2
+			) {
+				maxWheelForceAbs = slidingForce
+				sliding = true
+			}
+
+			let totalWheelForceAbs = Math.sqrt(forwardForceAbs ** 2 + sideForceAbs ** 2)
+			if (totalWheelForceAbs > maxWheelForceAbs) {
+				const scale = maxWheelForceAbs / totalWheelForceAbs
+				forwardForceAbs *= scale
+				sideForceAbs *= scale
+				sliding = true
+			}
+
+			if (sliding) {
+				wheel.mesh.material.color.setHex(0xff0000)
+			}
+			else {
+				wheel.mesh.material.color.setHex(0x00aa00)
+				let q = totalWheelForceAbs / maxWheelForceAbs
+				wheel.mesh.material.color.r = q
+			}
+
+			let forwardForceSigned = Math.sign(drive) * forwardForceAbs
+			let sideForceSigned = -Math.sign(sideVelocity) * sideForceAbs
+
+			let wheelForce = vec2.fromValues(
+				forwardDirection[0] * forwardForceSigned + axisDirection[0] * sideForceSigned,
+				forwardDirection[1] * forwardForceSigned + axisDirection[1] * sideForceSigned
+			)
+
+			let wheelRelativePosition = vec2.create()
+			chassisBody.vectorToWorldFrame(
+				wheelRelativePosition as [number, number],
+				wheelPosition as [number, number]
+			)
+
+			chassisBody.applyForce(
+				wheelForce as [number, number],
+				wheelRelativePosition as [number, number]
+			)
+
+			let centerOfRotation = vec2.copy(vec2.create(), chassisBody.position)
+			let corRadius = vec2.fromValues(
+				-chassisBody.velocity[1] / chassisBody.angularVelocity,
+				chassisBody.velocity[0] / chassisBody.angularVelocity
+			)
+			vec2.add(centerOfRotation, centerOfRotation, corRadius)
+
+			centerOfRotationMarker.position.set(
+				centerOfRotation[0],
+				centerOfRotation[1],
+				0.3
+			)
+
+			if (i == options.superSample - 1) {
+				arrow(globalWheelPosition, motion, 0.1, new THREE.Color(0x0000ff))
+				arrow(globalWheelPosition, wheelForce, 0.03, new THREE.Color(0xff00ff))
+				circle(globalWheelPosition, maxWheelForceAbs * 0.03, new THREE.Color(0xff00ff))
+
+				wheel.mesh.rotation.z = wheel.steering
+				wheel.mesh.position.x = wheel.position
+			}
+		}
 	}
-	else {
-		wheels[0].steering *= 0.9
-	}
-	wheels[1].thrust = keys.ArrowUp * wheelThrustForce - keys.ArrowDown * wheelBrakeForce
 
-	for (let wheel of wheels) {
-		const wheelAngle = chassisBody.angle + wheel.steering
-
-		let radius = vec2.create();
-		vec2.rotate(radius, wheel.position, [0, 0], chassisBody.angle);
-		const turningComponent = vec2.fromValues(
-			-chassisBody.angularVelocity * radius[1],
-			chassisBody.angularVelocity * radius[0]
-		)
-
-		let globalWheelPosition = vec2.create()
-		chassisBody.toWorldFrame(
-			globalWheelPosition as [number, number],
-			wheel.position as [number, number]
-		)
-
-		const motion = vec2.add(vec2.create(), chassisBody.velocity, turningComponent)
-		arrow(globalWheelPosition, motion, 0.1, new THREE.Color(0x0000ff))
-
-		const forwardDirection = vec2.fromValues(
-			Math.cos(wheelAngle),
-			Math.sin(wheelAngle),
-		)
-
-		const axisDirection = vec2.fromValues(
-			-forwardDirection[1],
-			forwardDirection[0]
-		)
-
-		const sideVelocity = vec2.dot(motion, axisDirection)
-		const sideSpeed = Math.abs(sideVelocity)
-
-		let forwardForceAbs = Math.abs(wheel.thrust)
-		let sideForceAbs = sideSpeed < wheelSlipThresholdSpeed ?
-			wheelDamping * sideSpeed : wheelSlidingForce
-
-		let sliding = false
-
-		let maxWheelForceAbs = wheelMaxGrippingForce
-		if (
-			sideSpeed > wheelSlipThresholdSpeed
-			|| forwardForceAbs ** 2 + sideForceAbs ** 2 > wheelMaxGrippingForce ** 2
-		) {
-			maxWheelForceAbs = wheelSlidingForce
-			sliding = true
-		}
-
-		let totalWheelForceAbs = Math.sqrt(forwardForceAbs ** 2 + sideForceAbs ** 2)
-		if (totalWheelForceAbs > maxWheelForceAbs) {
-			const scale = maxWheelForceAbs / totalWheelForceAbs
-			forwardForceAbs *= scale
-			sideForceAbs *= scale
-			sliding = true
-		}
-
-		if (sliding) {
-			wheel.mesh.material.color.setHex(0xff0000)
-		}
-		else {
-			wheel.mesh.material.color.setHex(0x00aa00)
-			let q = totalWheelForceAbs / maxWheelForceAbs
-			wheel.mesh.material.color.r = q
-		}
-
-		let forwardForceSigned = Math.sign(wheel.thrust) * forwardForceAbs
-		let sideForceSigned = -Math.sign(sideVelocity) * sideForceAbs
-
-		let wheelForce = vec2.fromValues(
-			forwardDirection[0] * forwardForceSigned + axisDirection[0] * sideForceSigned,
-			forwardDirection[1] * forwardForceSigned + axisDirection[1] * sideForceSigned
-		)
-		arrow(globalWheelPosition, wheelForce, 0.03, new THREE.Color(0xff00ff))
-		circle(globalWheelPosition, maxWheelForceAbs * 0.03, new THREE.Color(0xff00ff))
-
-		let wheelRelativePosition = vec2.create()
-		chassisBody.vectorToWorldFrame(
-			wheelRelativePosition as [number, number],
-			wheel.position as [number, number]
-		)
-
-		chassisBody.applyForce(
-			wheelForce as [number, number],
-			wheelRelativePosition as [number, number]
-		)
-
-		let centerOfRotation = vec2.copy(vec2.create(), chassisBody.position)
-		let corRadius = vec2.fromValues(
-			-chassisBody.velocity[1] / chassisBody.angularVelocity,
-			chassisBody.velocity[0] / chassisBody.angularVelocity
-		)
-		vec2.add(centerOfRotation, centerOfRotation, corRadius)
-
-		centerOfRotationMarker.position.set(
-			centerOfRotation[0],
-			centerOfRotation[1],
-			0.3
-		)
-
-		wheel.mesh.rotation.z = wheel.steering
-	}
-
-	const dt = lastTime == 0 ? 0.01 : time - lastTime
-	lastTime = time
-	world.step(dt * 0.4)
-	chassis.position.set(chassisBody.position[0], chassisBody.position[1], 0)
-	chassis.rotation.z = chassisBody.angle
+	chassisMesh.position.set(chassisBody.position[0], chassisBody.position[1], 0)
+	chassisMesh.rotation.z = chassisBody.angle
 	camera.position.set(chassisBody.position[0], chassisBody.position[1], 15)
+
 }
 
 run(update)
