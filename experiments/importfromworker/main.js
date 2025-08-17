4ddc9faac7c092ccba7057e1373389a3f05142ce
mqnc <mirko.kunze@web.de>
2025-08-17 16:08:35 +0000

    Deploying to gh-pages from  @ d4a251d493b14446e29effb39934cb22dc00acd3 🚀

--- experiments/importfromworker/main.js
+++ experiments/importfromworker/main.js
@@ -1,1 +1,1 @@
-var worker=new Worker("worker.js");import("./import.js").then((function(){console.log("imported in window")})).catch((function(){console.log("!!!! CATCH IN WINDOW !!!!")}));
+var worker=new Worker("worker.js");import("./import.js").then(function(){console.log("imported in window")}).catch(function(){console.log("!!!! CATCH IN WINDOW !!!!")});
